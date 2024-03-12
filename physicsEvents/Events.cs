6447110341db16196954840c8ddb44332765661a
spacewulf <kwolterstorff531@gmail.com>
2024-03-12 11:40:14 -0400

    Added the main console app.

---
+++ physicsEvents/Events.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Policy;
+
+namespace physicsEvents
+{
+    public class Events
+    {
+        public string Title { get; set; }
+        public string Speaker { get; set; }
+        public string Description { get; set; }
+        public string Location { get; set; }
+        public string Time { get; set; }
+        public int Duration { get; set; }
+        public Uri Uri { get; set; }
+    }
+}
---
+++ physicsEvents/Program.cs
@@ -0,0 +1,32 @@
+﻿using physicsEvents;
+using System;
+using System.ServiceModel;
+using System.ServiceModel.Syndication;
+using System.Xml;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string url = "http://events.umich.edu/group/1965/rss?v=2&html_output=true";
+        int eventNumber = 0;
+
+        XmlReader reader = XmlReader.Create(url);
+
+        SyndicationFeed feed = SyndicationFeed.Load(reader);
+
+        reader.Close();
+
+        foreach (SyndicationItem item in feed.Items)
+        {
+            eventNumber++;
+            Events events1 = new Events();
+            events1.Title = item.Title.Text.ToString();
+            events1.Description = item.Summary.Text.ToString();
+            events1.Uri = item.BaseUri;
+            Console.WriteLine(events1.Title);
+            // Console.WriteLine(events1.Description);
+            // Console.WriteLine(events1.Uri.ToString());
+        }
+    }
+}
